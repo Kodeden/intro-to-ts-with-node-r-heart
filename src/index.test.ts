ccdd2ae3d55cd482647e1343a794c24bff32071a
r-heart <rachaelkheart@gmail.com>
2023-12-15 19:13:43 -0800

    test: :test_tube: write test for `greet` function
    TODO: figure out why test code is seemingly correct, but the test is still failing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,9 @@
-import { describe, expect, it } from "vitest";
-import { add2Nums } from "./index.js";
+import { expect, test } from "vitest";
+import { greet } from "./index.js";
 
-describe("add2Nums", () => {
-  it("should return the sum of two numbers", () => {
-    expect(add2Nums(2, 3)).toBe(5);
-    expect(add2Nums(-1, 5)).toBe(4);
-    expect(add2Nums(0, 0)).toBe(0);
-  });
+test("greet function returns the correct greeting", () => {
+  const personName = "Amy";
+  const expectedGreeting = "Hello, Amy";
+  const result = greet(personName);
+  expect(result).toBe(expectedGreeting);
 });
