b34aa4cb4d875f640cd1521dc3ac32acf73a6705
r-heart <rachaelkheart@gmail.com>
2023-12-18 15:20:02 -0800

    refactor: :white_check_mark: update test of `greet` function

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import { greet } from "./index.js";
 
 test("greet function returns the correct greeting", () => {
   const user1 = { firstName: "Amy", lastName: "Jones" };
-  const expectedGreeting = "Hello, Amy Jones";
+  const expectedGreeting = "Hello, Amy Jones!";
   const result = greet(user1);
   expect(result).toBe(expectedGreeting);
 });
