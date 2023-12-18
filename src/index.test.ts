72f720e243ad3e7883bee02b3b5ebf0b1d1fca67
r-heart <rachaelkheart@gmail.com>
2023-12-18 15:25:28 -0800

    refactor: :recycle: export `interface Person` from `index.ts` and update test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from "vitest";
-import { greet } from "./index.js";
+import { greet, type Person } from "./index.js";
 
 test("greet function returns the correct greeting", () => {
-  const user1 = { firstName: "Amy", lastName: "Jones" };
+  const user1: Person = { firstName: "Amy", lastName: "Jones" };
   const expectedGreeting = "Hello, Amy Jones!";
   const result = greet(user1);
   expect(result).toBe(expectedGreeting);
