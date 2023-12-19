c9a0bb079d173d23d7bba8a760e4278bb72b6cfb
r-heart <rachaelkheart@gmail.com>
2023-12-18 19:43:45 -0800

    feat: :art: add readonly and optional properties to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 export interface Person {
+  readonly id: number;
   firstName: string;
   lastName: string;
+  age: number;
+  hobbies?: string[];
 }
 
 export function greet(person: Person): string {
   return `Hello, ${person.firstName} ${person.lastName}!`;
 }
+
+const me: Person = {
+  id: 1,
+  firstName: "John",
+  lastName: "Doe",
+  age: 42,
+};
+
+console.log(greet(me));
